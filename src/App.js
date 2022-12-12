3d84814efb1a77df8c163da7d475fb027308e038
Grzegorz Kolaty <grzegorzkolaty@gmail.com>
2022-12-12 17:20:43 +0100

    task list without interactions

--- src/App.js
+++ src/App.js
@@ -1,25 +1,34 @@
-import logo from './logo.svg';
-import './App.css';
+import Form from "./Form";
+import Tasks from "./Tasks";
+import Buttons from "./Buttons";
+import Section from "./Section";
+import Header from "./Header";
+import Container from "./Container";
+
+const tasks = [
+  { id: 1, content: "przejść na React", done: true },
+  { id: 2, content: "zjesc obiad", done: false },
+]
+
+let tasksHidingSwitch = false;
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <Container>
+      <Header title="Lista zadań React" />
+      <Section
+        title="Dodaj nowe zadanie"
+        body={<Form />}
+      />
+
+      <Section
+        title="Lista zadań"
+        body={<Tasks tasks={tasks} tasksHidingSwitch={tasksHidingSwitch} />}
+        extraHeaderContent={<Buttons tasks={tasks} tasksHidingSwitch={tasksHidingSwitch} />}
+      />
+    </Container>
+  )
 }
+
 
 export default App;
---
+++ src/Tasks/index.js
@@ -0,0 +1,30 @@
+import "./style.css"
+
+const Tasks = ({ tasks, tasksHidingSwitch }) => (
+  <ul className="list">
+    {tasks.map(task => (
+      <li
+        key={task.id}
+        className={`list__tasks
+          ${task.done && tasksHidingSwitch
+            ? "list__tasks--hidden"
+            : ""}`}>
+
+        <button className={`list__checkButton ${task.done
+          ? "list__checkButton--true"
+          : ""}`}>
+          ✔ </button>
+
+        <span className={`list__item ${task.done
+          ? " list__item--done"
+          : ""}`}>
+          {task.content} </span>
+
+        <button className={`list__removeButton`}>🗑</button>
+
+      </li>
+    ))}
+  </ul>
+);
+
+export default Tasks;
