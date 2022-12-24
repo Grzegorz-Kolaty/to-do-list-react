8ea2be622a70630fcf1976812ee72a6e6baed9fd
Grzegorz Kolaty <grzegorzkolaty@gmail.com>
2022-12-24 16:13:06 +0100

    Updates

---
+++ precache-manifest.8ad36eedf68630592a3680b4a0f43fe8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "52f266bcee054fdf9a55",
+    "url": "/to-do-list-react/static/css/main.84488fd6.chunk.css"
+  },
+  {
+    "revision": "52f266bcee054fdf9a55",
+    "url": "/to-do-list-react/static/js/main.52f266bc.chunk.js"
+  },
+  {
+    "revision": "2981538dd718ef902032",
+    "url": "/to-do-list-react/static/js/1.2981538d.chunk.js"
+  },
+  {
+    "revision": "fb3eafc590f585e52413",
+    "url": "/to-do-list-react/static/js/2.fb3eafc5.chunk.js"
+  },
+  {
+    "revision": "de4e60388a25d180fb9a",
+    "url": "/to-do-list-react/static/js/runtime~main.de4e6038.js"
+  },
+  {
+    "revision": "8ed017f6596ec19035e96d4996bd797d",
+    "url": "/to-do-list-react/index.html"
+  }
+];
