eabab6b6f14b24bcf1f604d61007ebd3a0063a26
Grzegorz Kolaty <grzegorzkolaty@gmail.com>
2023-01-02 23:46:44 +0100

    toggleHideDone interaction

--- src/Buttons/index.js
+++ src/Buttons/index.js
@@ -1,10 +1,10 @@
 import "./style.css";
 
-const Buttons = ({ tasks, hideDone }) => (
+const Buttons = ({ tasks, hideDone, toggleHideDone }) => (
   <div className="buttons">
     {tasks.length > 0 && (
       <>
-        <button className={`list__button`}>
+        <button onClick={toggleHideDone} className={`list__button`}>
           {hideDone ? "Pokaż" : "Ukryj"} ukończone
         </button>
         <button
